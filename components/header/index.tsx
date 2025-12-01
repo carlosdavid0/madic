9c55b35ae9bd6cfa7b0efa421c919df60288ee64
Carlos David <romao.carlosdavi@gmail.com>
2025-12-01 18:06:13 -0300

    style: melhorar responsividade da imagem no componente Header
    - Ajustar a classe da imagem do logo para garantir que ela seja responsiva em diferentes tamanhos de tela, utilizando lg:max-w-full e max-w-1/2.

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -6,9 +6,12 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center w-full py-4 mx-auto max-w-7xl px-4 2xl:px-0 pt-4">
       <div className="flex items-center">
-        <Image src="/logo-amarela.png" alt="Logo" width={180} height={180} 
-        className="lg:w-full w-1/2"
-        
+        <Image
+          src="/logo-amarela.png"
+          alt="Logo"
+          width={180}
+          height={180}
+          className="lg:max-w-full max-w-1/2"
         />
       </div>
       <Nav />
