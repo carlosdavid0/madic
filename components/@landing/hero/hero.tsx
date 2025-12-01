c703f6bedb904d0c98db0d63bfa820608d8d512f
Carlos David <romao.carlosdavi@gmail.com>
2025-12-01 18:02:10 -0300

    style: ajustar tamanhos de texto e botões no componente Hero
    - Reduzir o tamanho do título principal para 2xl para melhor legibilidade.
    - Ajustar tamanhos dos botões para text-base em telas pequenas e lg:text-lg em telas grandes, melhorando a responsividade.

--- components/@landing/hero/hero.tsx
+++ components/@landing/hero/hero.tsx
@@ -3,15 +3,15 @@ import { Button } from "@/components/ui/button";
 export default function Hero() {
   return (
     <section className="w-full max-w-4xl mx-auto px-4">
-      <h1 className="text-4xl lg:text-5xl font-normal text-center leading-tight">
+      <h1 className="text-2xl lg:text-5xl font-normal text-center leading-tight">
         O movimento que <span className="text-primary">impulsiona</span>
         <br />a arte digital{" "}
         <span className="font-medium">no interior do Ceará</span>
       </h1>
 
       <section className="flex justify-center mt-8 gap-x-4">
-        <Button variant={"secondary"} className="text-lg">Conheça</Button>
-        <Button className="text-lg">Saiba mais</Button>
+        <Button variant={"secondary"} className="text-base lg:text-lg">Conheça</Button>
+        <Button className="text-base lg:text-lg">Saiba mais</Button>
       </section>
     </section>
   );
