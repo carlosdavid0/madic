b545b60ed454759432800f6a78cb788a0e7a4a93
Carlos David <romao.carlosdavi@gmail.com>
2026-01-31 10:03:13 -0300

    feat: display various landing page sections including ranking, highlights, challenges, artists, supporters, and recent blog posts, and adjust the main layout to be a column.

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,28 +1,35 @@
+import { Apoiadores } from "@/components/@landing/apoiadores";
+import { ArtistasParaContratar } from "@/components/@landing/artistasParaContratar";
+import { Desafios } from "@/components/@landing/desafios";
+import { Destaques } from "@/components/@landing/destaques";
 import { ContentHeron } from "@/components/@landing/hero";
+import { Ranking } from "@/components/@landing/ranking";
+import { RecentesBlog } from "@/components/@landing/recentesBlog";
+
 
 export default function PublicPage() {
   return (
-    <section className="w-full overflow-y-hidden! h-[80vh] flex items-center justify-center">
+    <section className="w-full overflow-y-hidden! flex flex-col items-center justify-center">
       <ContentHeron />
 
-      {/* <section className="bg-magic-light w-full">
+      <section className="bg-magic-light w-full">
         <Ranking />
-      </section> */}
-      {/* <section className="bg-magic-dark w-full">
+      </section>
+      <section className="bg-magic-dark w-full">
         <Destaques />
-      </section> */}
-      {/* <section className="bg-magic-light w-full">
+      </section>
+      <section className="bg-magic-light w-full">
         <Desafios />
-      </section> */}
-      {/* <section className="bg-magic-dark w-full">
+      </section>
+      <section className="bg-magic-dark w-full">
         <ArtistasParaContratar />
-      </section> */}
-      {/* <section className="bg-magic-light w-full">
+      </section> 
+      <section className="bg-magic-light w-full">
         <Apoiadores />
-      </section> */}
-      {/* <section className="bg-magic-dark w-full">
+      </section>
+      <section className="bg-magic-dark w-full">
         <RecentesBlog />
-      </section> */}
+      </section>
     </section>
   );
 }
